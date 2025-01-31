74cd47238ec04f528f505506519ab9f030769157
Saryazdi-Saman <saman.s90@gmail.com>
2025-01-31 01:47:33 -0500

    submit button only opens otp dialog if the number has not changed

--- src/components/onboarding/sign-up/signUpForm.tsx
+++ src/components/onboarding/sign-up/signUpForm.tsx
@@ -17,6 +17,7 @@ const initalState: CredentialsActionResponse = {
 
 export const SignUpForm = () => {
     const [dialogOpen, setDialogOpen] = useState(false);
+    const [numberChanged, setNumberChanged] = useState(true);
 
     const [state, formAction, isPending] = useActionState(
         submitCredentials,
@@ -26,6 +27,7 @@ export const SignUpForm = () => {
     const [timer, { startCountdown, resetCountdown }] = useCountdown({ countStart: 60 })
 
     useEffect(() => {
+        setNumberChanged(false)
         if (state.errors?.phoneNumber) {
             setDialogOpen(true);
             startCountdown()
@@ -63,6 +65,15 @@ export const SignUpForm = () => {
                     required
                     autoComplete="tel-national"
                     defaultValue={state.inputs?.phoneNumber}
+                    onChange={(e)=> {
+                        console.log("e.target.value", e.target.value)
+                        console.log("state.inputs?.phoneNumber", state.inputs?.phoneNumber)
+                        if (state.inputs?.phoneNumber !== e.target.value) {
+                            setNumberChanged(true)
+                        } else if (state.inputs?.phoneNumber === e.target.value && e.target.value !== "") {
+                            setNumberChanged(false)
+                        }
+                    }}
                     className={clsx(
                         "w-full rounded-md border-2 border-gray-300 p-2 focus:border-blue-500 [&::-webkit-inner-spin-button]:appearance-none",
                         state.errors?.phoneNumber ? "border-red-500" : ""
@@ -80,12 +91,21 @@ export const SignUpForm = () => {
                 />
                 {state.errors?.password && <p className="text-red-500 text-sm">{state.errors.password[0]}</p>}
             </div>
-            <Button
-                disabled={isPending}
-                className="w-full"
-            >
-                {isPending ? <Loader2 className="animate-spin" /> : "Continue"}
-            </Button>
+            {numberChanged &&
+                <Button
+                    disabled={isPending}
+                    className="w-full" >
+                    {isPending ? <Loader2 className="animate-spin" /> : "Continue"}
+                </Button>
+            }
+            {!numberChanged &&
+                <Button
+                    type="button"
+                    disabled={isPending}
+                    onClick={() => { setDialogOpen(true) }}
+                    className="w-full bg-red-500" >
+                    Continue</Button>
+            }
             <OtpDialog
                 open={dialogOpen}
                 setOpen={setDialogOpen}
