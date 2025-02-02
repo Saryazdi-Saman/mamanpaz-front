e9ec7da1635cd9beeca1c9e03932ce1c3e481579
Saryazdi-Saman <saman.s90@gmail.com>
2025-02-01 20:54:33 -0500

    Create ScrollToTop component
    add ScrollToTop to pages to she the top of the page on redirects

--- src/app/(onboarding)/sign-up/page.tsx
+++ src/app/(onboarding)/sign-up/page.tsx
@@ -1,11 +1,11 @@
 import { SignUpForm } from "@/components/onboarding/sign-up/signUpForm";
-import { Toaster } from "@/components/ui/toaster";
+import ScrollToTop from "@/components/ui/scroll-to-top";
 import Link from "next/link";
 
 export default function SignUpPage() {
     return (
         <section className="min-h-screen md:px-4 lg:px-16 w-full bg-teal-50">
-            <Toaster />
+            <ScrollToTop />
             <h1 className="py-16 text-3xl md:text-4xl font-bold text-center">Enjoy 20% off for the first month-plus, your first week ships free!</h1>
             <div className="py-8 px-4 md:px-6 flex flex-col gap-y-1 w-full max-w-lg border-2 border-blue-50 justify-start items-start mx-auto relative bg-teal-50 rounded-md shadow-lg">
                 <SignUpForm />
