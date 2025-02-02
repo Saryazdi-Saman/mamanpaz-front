eeb4536aad2d60b3656615701af2d5cd31978aa4
Saryazdi-Saman <saman.s90@gmail.com>
2025-02-01 20:32:37 -0500

    initial commit

---
+++ src/app/(onboarding)/checkout/details/page.tsx
@@ -0,0 +1,10 @@
+export default function CheckoutDetailsPage() {
+    return (
+        <section className="min-h-screen md:px-4 py-16 lg:py-24 lg:px-16 w-full bg-teal-50">
+            <h1 className="text-3xl md:text-4xl font-bold text-center">Checkout</h1>
+            <p className="text-center text-base md:text-lg text-blue-400 px-4 text-balance">
+                Checkout details
+            </p>
+        </section>
+    )
+}
